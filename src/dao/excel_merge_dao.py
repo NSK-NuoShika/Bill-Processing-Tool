7e15cc6c8a6328ac91a69f02a690cb554dc81145
NSK <nsk_nuoshika@outlook.com>
2026-08-20 01:11:15 +0800

    feat: Add excel_merge module.

---
+++ src/dao/excel_merge_dao.py
@@ -0,0 +1,40 @@
+from openpyxl import Workbook, load_workbook
+from openpyxl.worksheet.worksheet import Worksheet
+
+
+class ExcelMergeDao:
+
+    def __init__(self, to_excel_path):
+        self._to_excel_path = to_excel_path
+        self._wb = self._create()
+
+
+    def merge(self, from_excel_path: str):
+        from_wb  = load_workbook(from_excel_path)
+
+        for from_ws in from_wb.worksheets:
+            to_ws = self._wb.create_sheet(from_ws.title)
+            self._copy_sheet(from_ws, to_ws)
+
+        from_wb.close()
+
+
+    def save(self) -> None:
+        self._wb.save(self._to_excel_path)
+        self._wb.close()
+
+
+    def _create(self) -> Workbook:
+        wb = Workbook()
+        wb.remove(wb.worksheets[0])
+
+        return wb
+
+
+    def _copy_sheet(self, from_ws: Worksheet, to_ws: Worksheet) -> None:
+        for row in from_ws.iter_rows():
+            for cell in row:
+                to_ws[cell.coordinate].value = cell.value
+
+        for m in from_ws.merged_cells.ranges:
+            to_ws.merge_cells(str(m))
--- src/service/bill_service.py
+++ src/service/bill_service.py
@@ -1,8 +1,11 @@
-from classifier.classifier import Classifier
-from service.classify_service import ClassifyService
+from src.dao.excel_merge_dao import ExcelMergeDao
+from src.model.bill_model import BillList
+from src.classifier.classifier import Classifier
+from src.service.classify_service import ClassifyService
 from src.service.excelsheet_service import ExcelSheetCreateService, ExcelSheetReadService
 from src.model.excelsheet_model import ExcelSheetReadModel, ExcelSheetCreateModel
-
+import uuid
+import os
 
 class BillService:
 
@@ -22,6 +25,8 @@ class BillService:
                 except StopIteration:
                     break
 
+        excel_write.save()
+
 
     def classify(self,
                  from_excel: ExcelSheetReadModel,
@@ -30,7 +35,14 @@ class BillService:
                  classifier: Classifier) -> None:
 
         excel_read = ExcelSheetReadService(from_excel)
-        excel_write = {x:ExcelSheetCreateService(ExcelSheetCreateModel(to_excel_path, x)) for x in category}
+        tmp_to_excel = dict()
+
+        for cate in category:
+            u = uuid.uuid4()
+            tmp_to_excel[cate] = (ExcelSheetCreateService(ExcelSheetCreateModel(f'{u}.xlsx', cate)),
+                                  f'{u}.xlsx')
+
+
         cls_service = ClassifyService(classifier)
 
         it = excel_read.load(30)
@@ -44,7 +56,14 @@ class BillService:
             res = cls_service.run(bl)
 
             for i in res:
-                excel_write[i.category].append(i)
+                t = BillList()
+                t.add(i)
+                tmp_to_excel[i.category][0].append(t)
 
-        for i in excel_write.values():
-            i.save()
+        excel_merge_dao = ExcelMergeDao(to_excel_path)
+        for k, v  in tmp_to_excel.items():
+            v[0].save()
+            excel_merge_dao.merge(v[1])
+            os.remove(v[1])
+
+        excel_merge_dao.save()
