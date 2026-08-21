86196fbcd738e2b2cc4b427fbc5581be6cc585a4
NSK <nsk_nuoshika@outlook.com>
2026-08-21 23:17:19 +0800

    fix: specify UTF-8 encoding for config file to resolve garbled text issue.

--- src/dao/config_dao.py
+++ src/dao/config_dao.py
@@ -9,10 +9,10 @@ class ConfigDao:
 
 
     def read(self):
-        with open(self.json_file, "r") as f:
+        with open(self.json_file, "r", encoding = 'utf-8') as f:
             return json.load(f)
 
 
     def write(self, data: dict[Any, Any]):
-        with open(self.json_file, "w") as f:
-            f.write(json.dumps(data, indent = 4, ensure_ascii = True))
+        with open(self.json_file, "w", encoding = 'utf-8') as f:
+            f.write(json.dumps(data, indent = 4, ensure_ascii = False))
